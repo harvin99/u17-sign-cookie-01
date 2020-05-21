9be84d7e698622ec49130889c2713348ff6aaf39
Glitch (hello-express) <none>
2020-05-21 04:49:04 +0000

    ✍️🌌 Checkpoint ./server.js:16337878/441 ./views/index.pug:16337878/393 ./views/books.pug:16337878/186 ./views/layout/common.pug:16337878/330

--- server.js
+++ server.js
@@ -11,14 +11,22 @@ const shortid = require('shortid')
 //for db
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-//Set db
-
+//Set default db
+db.defaults({ books: []})
+  .write()
+// Add a post
+db.get('books')
+  .push({ id: shortid.generate(), title: 'How to get smart money', description: 'Get a lot of money'})
+  .write()
 //Set view engine template
 app.set('view engine', 'pug')
-app.set('views', '/views')
+app.set('views', './views')
 //Router
 app.get('/', (req,res) => {
-  res.render('index)
+  res.render('index')
+})
+app.get('/books', (req, res) => {
+  res.render('books', {list : db.get('books').value()})
 })
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
