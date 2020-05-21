89197daed2796f2c74f19e2a759bfae0606038b6
Glitch (hello-express) <none>
2020-05-21 04:36:53 +0000

    🌁📼 Checkpoint ./server.js:16337878/1043 ./views/index.pug:16337878/291

--- server.js
+++ server.js
@@ -3,32 +3,24 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const shortid = require('shortid')
+//for db
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+//Set db
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
+//Set view engine template
+app.set('view engine', 'pug')
+app.set('views', '/views')
+//Router
+app.get('/', (req,res) => {
+  res.render('index)
+})
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  console.log("Your app is listening on port " + listener.address().port)
+})
