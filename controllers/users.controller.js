bf7eb92e396c70f46a9b6812ac696f3c9d795710
Glitch (hello-express) <none>
2020-05-24 03:53:53 +0000

    💾❣️ Checkpoint ./controllers/users.controller.js:16337878/1149 ./routes/users.router.js:16337878/899

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,7 +5,39 @@ const shortid = require('shortid')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 //Set default db
-db.defaults({ books: [], users: [], rent: []})
+db.defaults({ books: [], users: [], rents: []})
   .write()
 
-module.exports.getUser = 
+module.exports.getUser = (req, res) => {
+  res.render('users', {users: db.get('users').value()})
+}
+module.exports.createUser = (req, res) => {
+  res.render('create_user')
+}
+module.exports.postCreateUser = (req, res) => {
+  const user = {
+    id: shortid.generate(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone
+  }
+  db.get('users')
+    .push(user)
+    .write()
+  res.redirect('/users')
+}
+  
+module.exports.getUserId = (req, res) => {
+  const user = db.get('users').find({id: req.params.id}).value()
+  res.render('edit_user', {user: user})
+} 
+  
+  
+module.exports.postUserId = (req, res) => {
+  db.get('users')
+  .find({ id: req.params.id})
+  .assign({ name: req.body.name, email: req.body.emai, phone: req.body.phone})
+  .write()
+  res.redirect('/users')
+}
+module.exports
--- routes/users.router.js
+++ routes/users.router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const shortid = require('shortid')
+const userControllers = require('../controllers/users.controller')
 //for db
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -10,35 +11,11 @@ const db = low(adapter)
 db.defaults({ books: [], users: [], rents: []})
   .write()
 
-router.get('/', (req, res) => {
-  res.render('users', {users: db.get('users').value()})
-})
-router.get('/create_user', (req, res) => {
-  res.render('create_user')
-})
-router.post('/create_user', (req, res) => {
-  const user = {
-    id: shortid.generate(),
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone
-  }
-  db.get('users')
-    .push(user)
-    .write()
-  res.redirect('/users')
-})
-router.get('/:id', (req, res) => {
-  const user = db.get('users').find({id: req.params.id}).value()
-  res.render('edit_user', {user: user})
-})
-router.post('/:id', (req, res) => {
-  db.get('users')
-  .find({ id: req.params.id})
-  .assign({ name: req.body.name, email: req.body.emai, phone: req.body.phone})
-  .write()
-  res.redirect('/users')
-})
+router.get('/', userControllers.getUser)
+router.get('/create_user', userControllers.createUser)
+router.post('/create_user', userControllers.postCreateUser)
+router.get('/:id', userControllers.getUserId)
+router.post('/:id', userControllers.postUserId)
 router.get('/:id/delete', (req, res) => {
   db.get('users').remove({id: req.params.id}).write()
   res.redirect('/users')
