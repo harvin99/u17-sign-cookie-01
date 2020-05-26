b839e4da16511c243e579471f12b775f4ec8af0c
Glitch (hello-express) <none>
2020-05-26 01:35:24 +0000

    🙌🚃 Checkpoint ./controllers/users.controller.js:16337878/1375 ./validate/user.validate.js:16337878/481 ./validate/users.validate.js:16337878/40 ./routes/users.router.js:16337878/102

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const userControllers = require('../controllers/users.controller')
+const userValidate = require('../validate/users.validate')
+
 
 router.get('/', userControllers.getUser)
 router.get('/create_user', userControllers.createUser)
-router.post('/create_user', userControllers.postCreateUser)
+router.post('/create_user', userValidate.validatePostCreateUser , userControllers.postCreateUser)
 router.get('/:id', userControllers.getUserId)
 router.post('/:id', userControllers.postUserId)
 router.get('/:id/delete', userControllers.getUserIdToDelete)
