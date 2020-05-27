30139c67beb36b4fadec47204a61d8a749fa8c47
Glitch (hello-express) <none>
2020-05-27 09:13:22 +0000

    😬🎈 Checkpoint ./db.json:16337878/37 ./controllers/books.controller.js:16337878/2600 ./views/books.pug:16337878/498

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,46 +1,54 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const shortid = require('shortid')
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const shortid = require("shortid");
 //for db
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-//Set default db
-db.defaults({ books: [], users: [], rents: []})
-  .write()
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
 module.exports.getBook = (req, res) => {
-  res.render('books', {list : db.get('books').value()})
-}
+  const user = db.get('users').find({id : req.cookies.userId}).value()
+  console.log(user)
+  res.render("books", {
+    list: db.get("books").value(),
+    user: user
+  });
+};
 
 module.exports.createBook = (req, res) => {
-  res.render('create')
-}
+  res.render("create");
+};
 
 module.exports.postCreateBook = (req, res) => {
   const book = {
     id: shortid.generate(),
     title: req.body.title,
     description: req.body.description
-  }
-  db.get('books').push(book).write()
-  res.redirect('/books')
-}
+  };
+  db.get("books")
+    .push(book)
+    .write();
+  res.redirect("/books");
+};
 
 module.exports.getBookId = (req, res) => {
-  const book = db.get('books').find({id: req.params.id}).value()
-  res.render('edit', {book: book})
-}
+  const book = db
+    .get("books")
+    .find({ id: req.params.id })
+    .value();
+  res.render("edit", { book: book });
+};
 
 module.exports.postBookId = (req, res) => {
-  db.get('books')
-  .find({ id: req.params.id})
-  .assign({ title: req.body.title, description: req.body.description})
-  .write()
-  res.redirect('/books')
-}
+  db.get("books")
+    .find({ id: req.params.id })
+    .assign({ title: req.body.title, description: req.body.description })
+    .write();
+  res.redirect("/books");
+};
 
 module.exports.getBookIdToDelete = (req, res) => {
-  db.get('books').remove({id: req.params.id}).write()
-  res.redirect('/books')
-}
-
+  db.get("books")
+    .remove({ id: req.params.id })
+    .write();
+  res.redirect("/books");
+};
