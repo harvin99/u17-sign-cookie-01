3831ab7ed57abf0b442ea95cbbadb4666675bede
Glitch (hello-express) <none>
2020-05-28 07:28:32 +0000

    🚪👑 Checkpoint ./controllers/auth.controller.js:16337878/108 ./controllers/users.controller.js:16337878/66 ./db.json:16337878/465 ./public/custom.css:16337878/18

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,4 @@
+const md5 = require('md5')
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const shortid = require("shortid");
@@ -44,7 +45,7 @@ module.exports.postCreateUser = (req, res) => {
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
-    password: req.body.password,
+    password: md5(req.body.password),
     isAdmin: false
   };
   db.get("users")
